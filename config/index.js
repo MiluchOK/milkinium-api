eadee0fabdc9833cd80f9d4c31eee85b05d81d3d
alexey <amilyukov@amilyukovbranchio.local>
2018-05-22 17:18:15 -0700

    Experimenting with mocks

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 const isProduction = process.env.NODE_ENV === 'production';
 
-module.exports = isProduction ? require('./prod') : require('./dev');
+module.exports = require('./db.json');
 
--- controllers/__tests__/cases.test.js
+++ controllers/__tests__/cases.test.js
@@ -1,7 +1,31 @@
+const mockingoose = require('mockingoose').default;
 const controller = require('../cases')
 
 describe('Cases', function () {
     test('should be creatable', function() {
-        controller.create(mockReq, mockRes, mockNext)
+        mockReq = {
+            params: {
+                projectId: 1
+            },
+            body: {
+                
+            }
+        }
+
+        json_mock = jest.fn()
+        status_mock = jest.fn()
+            .mockReturnValue(json_mock)
+
+        mockRes = {
+            status: status_mock
+        }
+
+        mockNext = jest.fn()
+
+        mockingoose.Case.toReturn({name: 'foo'}, 'save')
+
+        const result = controller.create(mockReq, mockRes, mockNext)
+        expect(status_mock.mock.calls.length).toBe(1);
+        expect(mockNext.mock.calls.length).toBe(0);
     })
 });

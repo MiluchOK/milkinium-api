a38a791e20e01aea0a2488b5381fb5344dcabb72
alexey <amilyukov@amilyukovbranchio.local>
2018-06-01 10:52:29 -0700

    Adding integration tests

---
+++ integration/mongo-environment.js
@@ -0,0 +1,27 @@
+const NodeEnvironment = require('jest-environment-node');
+
+class MongoEnvironment extends NodeEnvironment {
+  constructor(config) {
+    console.log("In constructor")
+    super(config);
+  }
+
+  async setup() {
+    console.log('Setup MongoDB Test Environment');
+
+    this.global.__MONGO_URI__ = await global.__MONGOD__.getConnectionString();
+    this.global.__MONGO_DB_NAME__ = global.__MONGO_DB_NAME__;
+
+    await super.setup();
+  }
+
+  async teardown() {
+    console.log('Teardown MongoDB Test Environment');
+
+    await super.teardown();
+  }
+
+  runScript(script) {
+    return super.runScript(script);
+  }
+}
---
+++ integration/user.test.js
@@ -0,0 +1,5 @@
+// describe('User', function(){
+//     test('create', function(){
+//         console.log('foo')
+//     })
+// })

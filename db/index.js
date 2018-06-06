01ea731049d31abfcf069ff10b55e8e80c2f4d96
alexey <amilyukov@amilyukovbranchio.local>
2018-06-06 16:41:10 -0700

    Removing db folder

--- db/index.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const config = require('../config');
-
-module.exports = {
-    dbConnect: () => {
-        mongoose.Promise = require('bluebird');
-        return mongoose.connect(config.db_host);
-    }
-};

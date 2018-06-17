740900e3aa47472f8d9190c6944319e9376bc9b2
MiluchOK <milyukov.alexey@gmail.com>
2018-06-16 23:07:07 -0700

    Getting swagger in

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('./swagger.json');
 const routes = require('./routes');
 const errorHandler = require('./middleware/errorHandler');
 const auth = require('./middleware/authenticate');
@@ -9,6 +11,7 @@ const logger = require('./logger')('app_log');
 
 const app = express();
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use((auth.authMid).unless({path: ['/authenticate']}));

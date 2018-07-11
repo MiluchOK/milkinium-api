4122134b116ea40e8b9ec29b2293858f6df4512c
Alexey Milyukov <amilyukov@Alexeys-MBP.hsd1.ca.comcast.net>
2018-07-10 22:32:44 -0700

    Adding model tests for cases

---
+++ models/__tests__/cases.test.js
@@ -0,0 +1,26 @@
+const Case = require('../../models/cases');
+const mongoose = require('mongoose');
+
+const requiredFields = ['title', 'project']
+const validCaseData = {
+    title: 'Case1',
+    project: mongoose.Types.ObjectId()
+}
+
+describe('Cases', function () {
+    test('should allow to create a valid case', function(){
+        return new Case(validCaseData).validate()
+    })
+
+    requiredFields.forEach((field) => {
+        it(`should not allow to create a project without required '${field}' field`, (done) => {
+            let invalidCaseData = Object.assign({}, validCaseData)
+            delete invalidCaseData[field]
+            new Case(invalidCaseData).validate()
+            .catch((err) => {
+                expect(err.errors[field]).toBeTruthy()
+                done()
+            })
+        })
+   })
+});

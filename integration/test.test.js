afae39e387d8a07c9f73ee35a0352f76f534a4dd
Alexey Milyukov <amilyukov@Alexeys-MacBook-Pro.local>
2018-08-17 14:19:59 -0700

    More test refactoring

--- integration/test.test.js
+++ integration/test.test.js
@@ -27,80 +27,78 @@ beforeEach(() => {
     })
 })
 
-describe('Test', function(){
-    describe('index', function(){
-        test('should be protected', function(){
+
+describe('index', function(){
+    test('should be protected', function(){
+        return request(app)
+        .get(`/v1/runs/${run._id}/tests`)
+        .then(() => {
+            expect(authMock.authMid.mock.calls.length).toBe(1)
+        })
+    })
+
+    test('should return all tests for a run', function(){
+        return run.addCase(caze._id)
+        .then(() => {
             return request(app)
             .get(`/v1/runs/${run._id}/tests`)
-            .then(() => {
-                expect(authMock.authMid.mock.calls.length).toBe(1)
-            })
+            .expect(200)
         })
-
-        test('should return all tests for a run', function(){
-            return run.addCase(caze._id)
-            .then(() => {
-                return request(app)
-                .get(`/v1/runs/${run._id}/tests`)
-                .expect(200)
-            })
-            .then((response) => {
-                expect(response.body.tests).toHaveLength(1)
-                const targetTest = response.body.tests[0]
-                targetTest.id = targetTest.id.toString()
-                expect(targetTest).toEqual({
-                    id: targetTest.id,
-                    title: caze.title,
-                    case: caze._id.toString(),
-                    run: run._id.toString()
-                })
-            })
-        })
-
-    describe('create', function(){
-        test('should add tests to a run', function(){
-            const expectedTest = {
-                id: expect.any(String),
+        .then((response) => {
+            expect(response.body.tests).toHaveLength(1)
+            const targetTest = response.body.tests[0]
+            targetTest.id = targetTest.id.toString()
+            expect(targetTest).toEqual({
+                id: targetTest.id,
+                title: caze.title,
                 case: caze._id.toString(),
-                run: run._id.toString(),
-                title: caze.title
-            }
+                run: run._id.toString()
+            })
+        })
+    })
+})
+
+describe('create', function(){
+    test('should add tests to a run', function(){
+        const expectedTest = {
+            id: expect.any(String),
+            case: caze._id.toString(),
+            run: run._id.toString(),
+            title: caze.title
+        }
+        return request(app)
+        .post(`/v1/runs/${run._id}/tests`)
+        .send({cases: [caze._id]})
+        .expect(200)
+        .then((response) => {
+            expect(response.body).toEqual({tests: [expectedTest]})
+        })
+    })
+
+    describe('should not be able to add the same case twice', function(){
+        test('in the same batch', function(){
+            return request(app)
+            .post(`/v1/runs/${run._id}/tests`)
+            .send({cases: [caze._id, caze._id]})
+            .expect(400)
+            .then(response => {
+                expect(response.body).toEqual({error: "validation error"})
+            })
+        })
+    
+        test('as 2 separate requests', function(){
             return request(app)
             .post(`/v1/runs/${run._id}/tests`)
             .send({cases: [caze._id]})
             .expect(200)
-            .then((response) => {
-                expect(response.body).toEqual({tests: [expectedTest]})
-            })
-        })
-    })
-
-
-        describe('should not be able to add the same case twice', function(){
-            test('in the same batch', function(){
-                return request(app)
-                .post(`/v1/runs/${run._id}/tests`)
-                .send({cases: [caze._id, caze._id]})
-                .expect(400)
-                .then(response => {
-                    expect(response.body).toEqual({error: "validation error"})
-                })
-            })
-    
-            test('as 2 separate requests', function(){
+            .then(() => {
                 return request(app)
                 .post(`/v1/runs/${run._id}/tests`)
                 .send({cases: [caze._id]})
-                .expect(200)
-                .then(() => {
-                    return request(app)
-                    .post(`/v1/runs/${run._id}/tests`)
-                    .send({cases: [caze._id]})
-                    .expect(400)
-                })
-                .then(response => {
-                    expect(response.body).toEqual({error: errors.duplicateCasesForRun})
-                })
+                .expect(400)
+            })
+            .then(response => {
+                expect(response.body).toEqual({error: errors.duplicateCasesForRun})
             })
         })
     })

c22c23b099ed02b549dd1f2a2d3cc73147ce7f0c
Alex Milyukov <amilyukov@apple.com>
2019-09-05 13:43:50 -0700

    Using new parser for Mongo connection

--- config/db_connect.js
+++ config/db_connect.js
@@ -3,7 +3,7 @@ mongoose.Promise = require('bluebird')
 
 module.exports = {
     connect: function(mongoUrl){
-        return mongoose.connect(mongoUrl)
+        return mongoose.connect(mongoUrl, { useNewUrlParser: true })
     },
     teardown: function(){
         return mongoose.connection.close()
